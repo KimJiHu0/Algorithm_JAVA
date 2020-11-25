1822c8fdbc2b9dac701331ad8ff7112adf8c948c
KimJiHu0 <65142418+KimJiHu0@users.noreply.github.com>
2020-11-25 20:22:17 +0900

    Programmers Level1 서울에서 김서방 찾기

--- Level1/solution11.java
+++ Level1/solution11.java
@@ -6,7 +6,7 @@ public class solution11 {
 	 * [문제설명]
 	 * String형 배열 seoul의 element중 "Kim"의 위치 x를 찾아, "김서방은 x에 있다"는
 	 * String을 반환하는 함수, solution을 완성하세요.
-	 * seoul에 "Kim"은 오직 한번만 나타나며 잘못된 값이 입력되는 경우는 없습니다.
+	 * seoul에 "Kim"은 오직 한번만 나타나며 잘못된 값이 입력되는 경우는 없습니다. 
 	 * 
 	 * [제한사항]
 	 * seoul은 길이 1이상, 1000이하인 배열입니다.
