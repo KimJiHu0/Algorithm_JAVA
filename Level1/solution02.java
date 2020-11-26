e4465cb3a672dcd35a4aa9e632abaf0fabb269fb
KimJiHu0 <65142418+KimJiHu0@users.noreply.github.com>
2020-11-26 23:20:13 +0900

    Programmers Level1 제일 작은 수 빼기

--- Level1/solution02.java
+++ Level1/solution02.java
@@ -3,9 +3,11 @@ package Level1;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class solution02 { 
+public class solution02 {
 	
 	/*
+	 * 제일 작은 수 빼기
+	 * 
 	 * [문제설명]
 	 * 정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요.
 	 * 단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요.
